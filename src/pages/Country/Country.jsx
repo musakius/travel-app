3609aae264b0f88077800d1229a582cb2b29c284
Petr <musakius.work@gmail.com>
2021-03-14 23:56:52 +0300

    feat: add arrangement of blocks

--- src/pages/Country/Country.jsx
+++ src/pages/Country/Country.jsx
@@ -12,35 +12,40 @@ import Gallery from '../../components/Gallery';
 const Country = ({countries, countryName, setShowSearch}) => {
   useEffect(() => {
     setShowSearch(false);
+    window.scrollTo({top: 50, behavior: 'smooth'});
   }, []);
 
   const country = countries.find((el) => el.name.en.toLowerCase() === countryName);
 
   return (
     <main className={`${classes['container-country']} main`}>
-      <h1 className={classes.title}>
-        <TranslatableText
-          dictionary={{
-            russian: country.name.ru,
-            belarusian: country.name.be,
-            english: country.name.en
-          }}
-        />
-      </h1>
-      <div className={classes.widget}>
-        <LanguageConsumer>
-          {({language}) => (
-            <Widgets country={country} language={language} />
-          )}
-        </LanguageConsumer>
+      <div className="center">
+        <h1 className={classes.title}>
+          <TranslatableText
+            dictionary={{
+              russian: country.name.ru,
+              belarusian: country.name.be,
+              english: country.name.en
+            }}
+          />
+        </h1>
+        <div className={classes.content}>
+          <div className={classes.components}>
+            <div className={classes.gallery}>
+              <Gallery gallery={country.gallery} />
+            </div>
+            <div className={classes['video-and-map']}>
+              <Video video={country.video} />
+              <Map coordinates={country.coordinates} />
+            </div>
+          </div>
+          <div className={classes.widget}>
+            <LanguageConsumer>
+              {({language}) => <Widgets country={country} language={language} />}
+            </LanguageConsumer>
+          </div>
+        </div>
       </div>
-      <div className={classes.video}>
-        <Video video={country.video} />
-      </div>
-      <div className={classes.map}>
-        <Map coordinates={country.coordinates} />
-      </div>
-      <Gallery gallery={country.gallery} />
     </main>
   );
 };
