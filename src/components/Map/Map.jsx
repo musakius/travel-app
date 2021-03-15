f2db2032e1a38e8f55192bc704bdabe2fea19e00
Sasha <SashaZanko@mail.ru>
2021-03-15 16:28:03 +0300

    feat: add fs

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, {useCallback} from 'react';
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
 import './css/map.css';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import TranslatableText from '../TranslatableText/TranslatableText';
 
+document.addEventListener('keyDown', (e) => {
+  console.log(e.key);
+}) 
+
 const Map = ({coordinates}) => {
   const position = coordinates;
+  const handle = useFullScreenHandle();
  
   function mapChangeSize() {
     const mapWrapper = document.querySelector('.map-wrapper');
-    const map = document.querySelector('.leaflet-container');
     const button = document.querySelector('.map-button');
-    
+
     mapWrapper.classList.toggle('full-size');
-    map.classList.toggle('full-size');
-    button.classList.toggle('map-button--full-size');    
+    button.classList.toggle('map-button--full-size');
+
+    if(mapWrapper.classList.contains('full-size')) {
+      handle.enter();
+    } else {
+      handle.exit();
+    }  
   }
-  
+
+  document.addEventListener('fullscreenchange', (e) => {
+    const mapWrapper = document.querySelector('.map-wrapper');
+    const button = document.querySelector('.map-button');
+
+    if (!document.fullscreenElement) {
+      button.classList.remove('map-button--full-size');
+      mapWrapper.classList.remove('full-size');
+    } 
+  })
   
   return (
-  <>
-    <div className="card mb-3 map-wrapper">
+  <>   
+    <div className="card mb-3">
     <h4 className="card-header">
         <TranslatableText
                 dictionary={{
@@ -28,21 +47,30 @@ const Map = ({coordinates}) => {
                     english: 'Map'
                 }}
         />
-        </h4> 
-      <button className="map-button" onClick={mapChangeSize}></button>
+        </h4>
+        
       <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
-      <MapContainer center={position} zoom={4} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
+      
+      <FullScreen handle={handle}>
+        <button className="map-button" onClick={mapChangeSize}></button>
+        <div className="map-wrapper">
+        <MapContainer center={position} zoom={4} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+        </div>
+      </FullScreen>
+      
+      
     </div>
+    
     
   </>
 )  
