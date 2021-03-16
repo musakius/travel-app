62a2e9638fe28b136363c34c3eb78692a32d152c
Petr <musakius.work@gmail.com>
2021-03-16 11:31:09 +0300

    fix: responsive

--- src/components/Widgets/Weather/WeatherContainer.jsx
+++ src/components/Widgets/Weather/WeatherContainer.jsx
@@ -2,32 +2,27 @@ import React from 'react';
 import TranslatableText from '../../TranslatableText';
 import '../css/owfont-regular.css';
 
+const WeatherContainer = ({data, country}) => {
+  const {weather, main} = data;
 
-const WeatherContainer = ({ data, country }) => {
-    const { weather, main } = data;
+  const img = `owf owf-${weather[0].id} owf-2x`;
 
-    const img = `owf owf-${weather[0].id} owf-2x`;
-
-    return (
-        <div className="card-body">
-            <h5 className="card-title">
-            <TranslatableText
-                dictionary={{
-                    russian: country.capital.ru,
-                    belarusian: country.capital.be,
-                    english: country.capital.en
-                }}
-            />
-            </h5>
-            <i className={img}></i>
-            <h4 className="card-text">
-                {
-                    `${main.temp.toFixed(0)}°C`
-                }
-            </h4>
-            <di className="card-text">{weather[0].description}</di>           
-        </div>
-    );
-}
+  return (
+    <div className="card-body">
+      <h5 className="card-title">
+        <TranslatableText
+          dictionary={{
+            russian: country.capital.ru,
+            belarusian: country.capital.be,
+            english: country.capital.en
+          }}
+        />
+      </h5>
+      <i className={img}></i>
+      <h4 className="card-text">{`${main.temp.toFixed(0)}°C`}</h4>
+      <div className="card-text">{weather[0].description}</div>
+    </div>
+  );
+};
 
 export default WeatherContainer;
--- src/components/Widgets/Widgets.jsx
+++ src/components/Widgets/Widgets.jsx
@@ -2,26 +2,26 @@ import React from 'react';
 import Weather from './Weather/Weather';
 import DateTime from './Date/DateTime';
 import Rate from './Rate/Rate';
-import { LanguageConsumer } from '../../context';
+import {LanguageConsumer} from '../../context';
 
 /**Country widgets */
 
-const Widgets = ({ country }) => {
-    const capital = country.capital.en;
+const Widgets = ({country}) => {
+  const capital = country.capital.en;
 
-    return (
-        <LanguageConsumer>
-        {({language}) => (
-            <div className="card border-light mb-3">
-                <div className="card-body">
-                    <DateTime country={ country } language={ language } />
-                    <Weather country={ country } capital={ capital } language={ language } />
-                    <Rate currency={country.currency} language={ language } />
-                </div>
-            </div>
-        )}
-      </LanguageConsumer>
-    );
-}
+  return (
+    <LanguageConsumer>
+      {({language}) => (
+        <div className="card border-light mb-3">
+          <div className="card-body p-0">
+            <DateTime country={country} language={language} />
+            <Weather country={country} capital={capital} language={language} />
+            <Rate currency={country.currency} language={language} />
+          </div>
+        </div>
+      )}
+    </LanguageConsumer>
+  );
+};
 
 export default Widgets;
