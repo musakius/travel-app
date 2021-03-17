2013f292bee0a313413d6fb452b0098b716f5101
Petr <musakius.work@gmail.com>
2021-03-17 17:28:43 +0300

    fix: add HashRouter

--- src/index.js
+++ src/index.js
@@ -4,17 +4,16 @@ import App from './components/App';
 import {Provider} from 'react-redux';
 import {LanguageProvider} from './context';
 import store from './redux/store';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import './index.scss';
 
 ReactDOM.render(
   <Provider store={store}>
     <LanguageProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </LanguageProvider>
   </Provider>,
   document.getElementById('root')
 );
-
